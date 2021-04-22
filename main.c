6ba00909c17e881cd18907307aa6ba99770dce43
Maxime Chantemargue <maxime.chantemargue@heig-vd.ch>
2021-04-22 08:50:26 +0200

    add project to repo

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : main.c
+ Auteur(s)      : Guilan Mbayo, Mehdi Sahli, Maxime Chantemargue
+ Date creation  : 03.04.2021
+
+ Description    : Programme principal
+
+ Remarque(s)    : -
+
+ Compilateur    : Mingw-w64 gcc 8.1.0
+ -----------------------------------------------------------------------------------
+*/
+#include <stdlib.h>
+
+int main(void) {
+
+    return EXIT_SUCCESS;
+}
